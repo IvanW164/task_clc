bbab06e15cb10e5e91427df9169cb0714a576b00
Anastasiia <venanastasiia@gmail.com>
2022-04-10 19:37:06 +0300

    Введение и вывод данных

---
+++ view.py
@@ -0,0 +1,11 @@
+def view_data(data):
+    print(f'Результат выполнения действия = {data}')
+
+def get_value():
+    return float(input('value = '))
+
+def get_complex_value():
+    return complex(input('value = '))
+
+def get_action():
+    return input('action = ')
